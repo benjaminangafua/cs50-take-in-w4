0b2b31025a59fa02b0d35276140ffc24311889b2
benjaminangafua <benjaminarkutl2017@gmail.com>
2021-07-20 16:58:53 +0000

    Update from week 4

---
+++ jpeg.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdint.h>
+
+typedef uint8_t BYTE;
+int main(int argc, char *argv[])
+{
+    //get two commandline argument
+    if(argc != 2){return 1;} 
+    
+    //open file
+    FILE *file = fopen(argv[1], "r");
+    if(!file){return 1;}
+
+
+    BYTE byte[3];
+    fread(byte, sizeof(BYTE), 3, file);
+    (byte[0] == 0xff && byte[1] == 0xd8 && byte[2] == 0xff) ? printf("maybe\n") : printf("No\n");
+}
