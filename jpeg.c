c195bd62ab1cf24cf45ca516d3208449b5753a35
benjaminangafua <benjaminarkutl2017@gmail.com>
2021-08-02 23:39:58 +0000

    Comment insertion

--- jpeg.c
+++ jpeg.c
@@ -10,8 +10,7 @@ int main(int argc, char *argv[])
     //open file
     FILE *file = fopen(argv[1], "r");
     if(!file){return 1;}
-
-
+    //validate the first three bytes of a jpeg file
     BYTE byte[3];
     fread(byte, sizeof(BYTE), 3, file);
     (byte[0] == 0xff && byte[1] == 0xd8 && byte[2] == 0xff) ? printf("maybe\n") : printf("No\n");
